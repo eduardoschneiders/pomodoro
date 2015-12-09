bfd313659ba49c9a3d56757ac3a566c332abd23a
eduardo.schneiders <eduardo.schneiders@cwi.com.br>
2015-12-09 09:06:46 -0200

    Use Pinterest API

--- server.py
+++ server.py
@@ -7,13 +7,25 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+
   state = request.args.get('state')
   code = request.args.get('code')
 
   query_params = '?grant_type=authorization_code&client_id=client_id&client_secret=client_secret&code=' + code
 
   response = requests.post('https://api.pinterest.com/v1/oauth/token' + query_params)
-  return response.text + state + code
+  parsed_response =  json.loads(response.text)
+  access_token = parsed_response['access_token']
+  print(access_token)
+
+  query_params = '?access_token=' + access_token + '&fields=id%2Clink%2Cnote%2Curl%2Cattribution%2Cmedia%2Cboard%2Coriginal_link%2Cmetadata%2Ccolor%2Ccounts%2Ccreated_at%2Ccreator%2Cimage'
+  response = requests.get('https://api.pinterest.com/v1/me/pins' + query_params)
+  parsed_response =  json.loads(response.text)
+
+  for pin in parsed_response['data']:
+    print(pin['image']['original']['url'])
+
+  return response.text
 
 @app.route('/test')
 def test():
